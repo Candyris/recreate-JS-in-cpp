5a83e2e5e3b213e89936be46d13e4a257f102148
Candyrizz <129657681+Candyris@users.noreply.github.com>
2024-09-22 19:42:24 +0530

    Update object_vec.cpp

--- JS/datatype/object_vec.cpp
+++ JS/datatype/object_vec.cpp
@@ -18,7 +18,7 @@ public:
     {
         std::cout << "derived : value -> " << data << std::endl;
     }
-    double data;
+    double data = 0;
 };
 
 using Object = std::vector<std::unique_ptr<base>>;
@@ -32,7 +32,7 @@ int main(void)
 
     std::random_device rd;
     std::mt19937 machine(rd());
-    std::uniform_real_distribution<double> urf(1,100);
+    std::uniform_real_distribution<double> urf(1, 100);
     for (size_t i = 0; i < size; i++)
     {
         ranObject.push_back(std::make_unique<derived>());
@@ -45,7 +45,19 @@ int main(void)
         object[i] = std::move(ranObject[i]);
     }
 
-    for (const std::unique_ptr<base> &ptr : object)
+    for (size_t i = 0; i < size; i++)
+    {
+        if (ranObject[i].get() == nullptr)
+        {
+            std::cout << "My ranObject at " << i << " is null" << std::endl;
+            
+        }
+    }
+    ranObject.clear();
+
+    std::cout << "Size of previous inhertVector: " << ranObject.size() << std::endl;
+
+    for (const std::unique_ptr<base>& ptr : object)
         ptr.get()->print();
     // std::cout<<"Hello world!"<<std::endl;
 
